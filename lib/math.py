0b770ba166f90a5ed62daab867bba1a356590b0b
lorne <1991wangliang@gmail.com>
2025-10-17 09:59:28 +0800

    init

---
+++ lib/math.py
@@ -0,0 +1,17 @@
+class Math:
+    @staticmethod
+    def add(a: int, b: int) -> int:
+        return a + b
+
+    @staticmethod
+    def sub(a: int, b: int) -> int:
+        return a - b
+
+    @staticmethod
+    def mul(a: int, b: int) -> int:
+        return a * b
+
+    @staticmethod
+    def div(a: int, b: int) -> float:
+        return a / b
+
